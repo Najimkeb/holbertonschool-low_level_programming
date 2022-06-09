8b4c0799178eb88b00460b9cac13a3b9de0caa33
Najimkeb <najimkeb31@gmail.com>
2022-06-09 08:30:46 -0700

    Task 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -6,9 +6,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 char c;
-for (c = 'a' ; c <= 'z' ; c++) putchar (c); 
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
 return (0);
 }
