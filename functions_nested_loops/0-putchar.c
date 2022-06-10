3097839ea2aa1f97e5136250e82edbc2dd9f0980
Najimkeb <najimkeb31@gmail.com>
2022-06-10 11:12:10 -0700

    0task

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "_putchar"
+
+/**
+ * main - Prints "_putchar"" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+_putchar('_putchar');
+_putchar('\n')
+return (0);
+}
