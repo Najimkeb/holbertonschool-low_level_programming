561f4c6c1409e643efc700766193751a62d74201
Najimkeb <najimkeb31@gmail.com>
2022-06-11 08:20:08 -0700

    2task

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@ void print_alphabet_x10(void)
 {
 char c;
 int n
-n = 0;
-for (n < 10);
+for (n = 0; n <10; n++);
 {
 for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(n);
 }
-n++;
 _putchar('\n');
 }
+}
+
