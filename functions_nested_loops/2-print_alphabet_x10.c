4fe6249218754c98c0e5f3a3351b2178ee71c060
Najimkeb <najimkeb31@gmail.com>
2022-06-11 08:02:00 -0700

    2task

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*print_alphabet - prints the alphabet in lowercase, followed by a new line.
+*
+*/
+void print_alphabet_x10(void);
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
