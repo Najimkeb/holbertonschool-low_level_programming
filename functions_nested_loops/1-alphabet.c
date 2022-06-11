2edb38909523b6dc8311aa016b9266c08c48fd61
Najimkeb <najimkeb31@gmail.com>
2022-06-11 05:48:22 -0700

    1task

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,20 +1,12 @@
-/*
- * File: 1-alphabet.c
- *
- */
+#include "main.h"
 
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 /**
- * main - Entry point
+ * main - check the code
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
-void print_alphabet(void)
+int main(void)
 {
-char c;
-for (c = 'a' ; c <= 'z' ; c++)
-putchar(c);
-putchar('\n');
+    print_alphabet();
+    return (0);
 }
