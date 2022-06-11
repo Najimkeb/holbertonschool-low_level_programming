22f1d7415c064b14168b594123b4e2d99b9d49d7
Najimkeb <najimkeb31@gmail.com>
2022-06-11 05:53:54 -0700

    1task

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -9,8 +9,10 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+char c;
+for (c ='a'; c <= 'z'; c++)
+_putchar(c)
+_putchar('\n');
 }
