2adf06be7f081b9aa07ea8887a477cc6e5fc4688
Najimkeb <najimkeb31@gmail.com>
2022-06-14 06:44:24 -0700

    task3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_number - check the code
+ *
+ */ 
+void print_numbers(void);
+
+{
+	int a, b
+		for(a = 0; a <= 9; a++)
+			for(b = 0; b <= 9; b++)
+				_putchar(a)
+					_putchar('\n')
+}
