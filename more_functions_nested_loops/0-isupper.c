1d72103f13006aa1284bdf0ab975d5c6c8cc73a6
Najimkeb <najimkeb31@gmail.com>
2022-06-14 05:33:13 -0700

    task0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - Cfr description
+ * _isupper.c - Cfr description
  * @c: input character
  * Description: checks for lower case character
  * Return: 1 if is lowercase and 0 otherwise
