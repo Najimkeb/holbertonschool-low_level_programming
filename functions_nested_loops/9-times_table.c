6bfa370caaf8409e2eaf01ff0f23bd46f16adb68
Najimkeb <najimkeb31@gmail.com>
2022-06-14 01:43:00 -0700

    task9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -3,17 +3,20 @@
 
 /**
  * void times_table(void) - check the code
- *
+ * resultat
+ * return à zero
  */
 
 void times_table(void);
 {
 	int n, i;
 
-	printf("imprime une table ");
-	scanf("%d", &n);
-	for (i = 1; i <= 10; ++i)
-	{
-		printf("%d * %d = %d \n", n, i, n * i);
-	}
+	for (n = 9; n++)
+
+		for (i = 1; i <= 10; ++i)
+		{
+			resultat = n * i;
+			printf("%d\n", resultat);
+		}
+	return (0);
 }
