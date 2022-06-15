2abf032c5a10c68eeda7938ef45df866022febf8
Najimkeb <najimkeb31@gmail.com>
2022-06-15 01:20:13 -0700

    task5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_numbers- print number
+ */
+
+void print_numbers(void)
+{
+	int a, n;
+
+	for (a = 0; a <= 10; a++)
+		for (n = 0; n <= 10; n++)
+		_putchar(a);
+	_putchar('\n');
+}
