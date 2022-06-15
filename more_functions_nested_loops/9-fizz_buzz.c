8a48b5c36929b990f8a940872db95dbdcd55e5c9
Najimkeb <najimkeb31@gmail.com>
2022-06-15 08:40:38 -0700

    task9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,22 +14,22 @@ int main(void)
 	for (a = 1; a <= 100; a++)
 	{
 		if ((a % 3 == 0 && (a % 5) == 0))
-				printf(" FizzBuzz");
+			printf(" FizzBuzz");
 
-				else if (a % 5 == 0)
+		else if (a % 5 == 0)
 
-				printf(" Buzz");
-				else if (a % 3 == 0)
+			printf(" Buzz");
+		else if (a % 3 == 0)
 
-				printf(" Fizz");
-				else
+			printf(" Fizz");
+		else
 
-				printf("%d", a);
+			printf("%d", a);
 
-				if (a == 100)
-				continue;
-				printf(" ");
-				}
-				printf("\n");
-				return (0);
+		if (a == 100)
+			continue;
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
 }
