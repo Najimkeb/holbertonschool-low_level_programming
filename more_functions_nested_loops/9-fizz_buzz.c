6d4e8b7d1e6da4ac7883e1a2dce7f29f411249e5
Najimkeb <najimkeb31@gmail.com>
2022-06-15 05:57:51 -0700

    task9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *main - check the code
+ *
+ */
+
+int main(void)
+{
+	int a;
+	for (a =1; a <= 100; a++)
+
+		if(a%3==0)
+		{
+			_putchar("Fizz\n");
+		}
+		else if (a%5==0)
+		{
+			_putchar("Buzz\n");
+		}
+		else
+		{
+			(a%3==0; a%5==O)
+				_putchar("FizzBuZZ");
+		}
+}
