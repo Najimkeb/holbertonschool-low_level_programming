ab92bb378db228d26db2c475d8a2b117c946bb7d
Najimkeb <najimkeb31@gmail.com>
2022-06-15 02:27:31 -0700

    task5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,14 @@ void more_numbers(void)
 	int a, n;
 
 	for (a = 0; a <= 10; a++)
-		for (n = 0; n <= 10; n++)
-		_putchar(a);
-	_putchar('\n');
-}
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
+	}
