539124c96bcd9a4b1aea278d37460e19de7b858f
Najimkeb <najimkeb31@gmail.com>
2022-06-16 04:25:19 -0700

    task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "mai.h"
+#include <stdio.h>
+
+/**
+ *  reset_to_98 - check the code
+ *@n: pointer
+ *
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+
