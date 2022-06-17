e0077423b6cd8ee23e01e3bc5e10ab5e195b4eaf
Najimkeb <najimkeb31@gmail.com>
2022-06-17 11:39:01 -0700

    task6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,8 +2,10 @@
 #include<stdio.h>
 
 /**
- *   puts2 - prints every other character of a string 
+ * puts2 - prints every other character of a  string to stdout
  * @str: string to print
+ *
+ * Return: void
  */
 
 void puts2(char *str)
