b6b605f06c008d47dd55e827d4b0c5be543afef0
Najimkeb <najimkeb31@gmail.com>
2022-06-17 11:23:29 -0700

    task5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,17 +7,20 @@
  */
 void rev_string(char *s)
 {
-int a = 0, b = 0;
-	char tmp;
+	int a = 0, b = 0;
+	char str[500];
 
-	while (s[b++])
-		a++;
-
-	for (a = b - 1; b >= a / 2; b--)
+	while (*(s + a))
 	{
-		tmp = s[b];
-		s[a] = s[a - b - 1];
-		s[a - b - 1] = tmp;
+		*(str + a) = *(s + a);
+		i++;
+	}
+	i = a - 1;
+	while (a >= 0)
+	{
+		*(s + a) = *(str + b);
+		b++;
+		a--;
 	}
 }
 
