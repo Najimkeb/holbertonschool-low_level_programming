1442f16a84a6cb1861b85813559e5e5d37511457
Najimkeb <najimkeb31@gmail.com>
2022-06-17 11:29:25 -0700

    task5

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string 
+ * @str: string to print
+ */
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (*(str + a))
+	{
+		_putchar(*(str + a));
+		a = a + 2;
+	}
+	_putchar('\n');
+}
+
