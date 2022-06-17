dfda248ca7152acf732513b2f81e5488f44a504f
Najimkeb <najimkeb31@gmail.com>
2022-06-17 08:28:03 -0700

    task4

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * rev_string - Reverses a string
+ * @s: modified
+ */
+void rev_string(char *s)
+{
+	int d, b, c;
+	char *a, aux;
+
+	a = s;
+
+	while (s[c] != '\0')
+	{
+		b++;
+	}
+
+	for (c = 1; c < b; c++)
+	{
+		a++;
+	}
+
+	for ( d= 0; d < (b / 2); d++)
+	{
+		aux = s[d];
+		s[d] = *a;
+		*a = aux;
+		a--;
+	}
+}
+
