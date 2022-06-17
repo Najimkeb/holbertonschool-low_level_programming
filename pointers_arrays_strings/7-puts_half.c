27edc48d976720c67fb50b71d1183c2878dfa2e1
Najimkeb <najimkeb31@gmail.com>
2022-06-17 11:43:38 -0700

    task7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int a, b = 0;
+
+	while (*(str + a))
+		i++;
+	b = a / 2;
+	if (a % 2)
+		b += 1;
+	while (b < i)
+	{
+		_putchar(*(str + b));
+		b++;
+	}
+	_putchar('\n');
+}
