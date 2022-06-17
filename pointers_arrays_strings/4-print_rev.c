41563f5a87ba2966d13d21ad727c49f50e50ed61
Najimkeb <najimkeb31@gmail.com>
2022-06-17 07:24:17 -0700

    task4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string, followed by a new line
+ * @str:variable
+ */
+
+void print_rev(char *s);
+{
+	int a = 0, b;
+
+	while (s [b++])
+		a++;
+
+	for ( b = a -1; b => 0; b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,3 +23,4 @@ void swap_int(int *a, int *b);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void print_rev(char *s);
