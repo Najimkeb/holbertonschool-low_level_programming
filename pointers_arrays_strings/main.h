a539acc99aa3fcae8288693edbf11d31dbe90c58
Najimkeb <najimkeb31@gmail.com>
2022-06-21 01:30:15 -0700

    1-memcpy.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -32,3 +32,4 @@ char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
