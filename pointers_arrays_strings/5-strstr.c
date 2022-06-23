8875410e08d3cae45eed91c420b36010a9f4fe24
Najimkeb <najimkeb31@gmail.com>
2022-06-23 01:46:47 -0700

    5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strstr - check the code
+ *@a:variable
+ @b:variable
+ * Return: Always 0.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	int a = 0, b = 0;
+
+	while (haystack[a])
+	{
+		while (needle[b])
+		{
+			if (haystack[a + b] != needle[b])
+			{
+				break;
+			}
+
+			b++;
+		}
+
+		if (needle[b] == '\0')
+		{
+			return (haystack + a);
+		}
+
+		a++;
+	}
+
+	return ('\0');
+}
