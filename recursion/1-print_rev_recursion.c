291c8a75f2153c21cc1b55ec61cc5cac6a6bb397
Najimkeb <najimkeb31@gmail.com>
2022-06-24 08:17:26 -0700

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -12,10 +12,10 @@ void _print_rev_recursion(char *s)
 
 	if (*s == '\0')
 	{
-		return (s);
+
+		s++;
+		_print_rev_recursion(s);
+		s--;
+		_putchar (*s);
 	}
-	s++;
-	_print_rev_recursion(s);
-	s--;
-	_putchar (*s);
 }
