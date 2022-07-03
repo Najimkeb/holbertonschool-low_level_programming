b7f0f94142cb2fd0b61cee4a3eb21dbd7b219105
Najimkeb <najimkeb31@gmail.com>
2022-07-03 00:37:08 -0700

    task 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
-  * calloc -  allocates memory for an array, using malloc
+  * _calloc -  allocates memory for an array, using malloc
   * @nmemb: number of members
   * @size: size
   *
