12840cd753dfe6f3c7cc18dc957337f2ddc88d72
Najimkeb <najimkeb31@gmail.com>
2022-07-03 00:25:24 -0700

    task 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,15 @@
+#include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
-  * malloc_checked - Allocates memory
-  * @b: the size to allocate
-  *
-  * Return: Nothing.
-  */
+ * malloc_checked - Main entry
+ * @b: The character to print
+ * Return: void
+ */
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	void *p = malloc(b);
 
-	p = malloc(b);
 
 	if (p == NULL)
 		exit(98);
