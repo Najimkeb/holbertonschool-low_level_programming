c5844061cb5428f57545f4d4fae21b95cb43aed0
Najimkeb <najimkeb31@gmail.com>
2022-07-04 03:03:24 -0700

    task 0

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTERS.H
-#define FUNCTION_POINTERS.H
+#ifndef FUNCTION_POINTERS .H
+#define FUNCTION_POINTERS .H
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
-#endif /* FUNCTION_POINTERS.H */
+#endif /* FUNCTION_POINTERS .H */
