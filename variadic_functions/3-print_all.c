2a0aa1de9e171ca1e9405af2c39698ff9f61fe48
Najimkeb <najimkeb31@gmail.com>
2022-07-08 09:27:32 -0700

    task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,7 +1,113 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
+
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_char - Prints a char.
+ * @arg: A list of arguments pointing to
+ *       the character to be printed.
  */
+void print_char(va_list arg)
+{
+	char a;
 
+	a = va_arg(arg, int);
+	printf("%c", a);
+}
 
+/**
+ * print_int - Prints an int.
+ * @arg: A list of arguments pointing to
+ *       the integer to be printed.
+ */
+void print_int(va_list arg)
+{
+	int b;
+
+	b = va_arg(arg, int);
+	printf("%d", b);
+}
+
+/**
+ * print_float - Prints a float.
+ * @arg: A list of arguments pointing to
+ *       the float to be printed.
+ */
+void print_float(va_list arg)
+{
+	float b;
+
+	b = va_arg(arg, double);
+	printf("%f", b);
+}
+
+/**
+ * print_string - Prints a string.
+ * @arg: A list of arguments pointing to
+ *       the string to be printed.
+ */
+void print_string(va_list arg)
+{
+	char *str;
+
+	str = va_arg(arg, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+
+	printf("%s", str);
+}
+
+/**
+ * print_all - Prints anything, followed by a new line.
+ * @format: A string of characters representing the argument types.
+ * @...: A variable number of arguments to be printed.
+ *
+ * Description: Any argument not of type char, int, float,
+ *              or char * is ignored.
+ *              If a string argument is NULL, (nil) is printed instead.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int c = 0, d = 0;
+	char *separator = "";
+	printer_t funcs[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+
+	va_start(args, format);
+
+	while (format && (*(format + c)))
+	{
+		d = 0;
+
+		while (d < 4 && (*(format + c) != *(funcs[d].symbol)))
+			d++;
+
+		if (d < 4)
+		{
+			printf("%s", separator);
+			funcs[d].print(args);
+			separator = ", ";
+		}
+
+		c++;
+	}
+
+	printf("\n");
+
+	va_end(args);
+}
