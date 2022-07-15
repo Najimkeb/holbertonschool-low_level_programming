e8790f499b5bc0df487f4f29c2e07829c0bd911e
Najimkeb <najimkeb31@gmail.com>
2022-07-15 02:16:18 -0700

    task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,21 +3,19 @@
 /**
  * size_t print_list - prints all the elements of a list_t list
  * @h: pointor
+ *
 Return: number of nodes in h
 */
 
 size_t print_list(const list_t *h)
-{
+	{
 	size_t node = 0;
 
-	while (h != NULL);
+	while (h != NULL)
 	{
 		if (h->str != NULL)
-
 			printf("[0] (nil)\n");
-
 		else
-
 			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
@@ -25,5 +23,4 @@ size_t print_list(const list_t *h)
 	}
 
 	return (node);
-
 }
