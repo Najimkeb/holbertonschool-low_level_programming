4a56c481502a4e869677b2501fedbb2b5f1f9b8b
Najimkeb <najimkeb31@gmail.com>
2022-07-15 02:38:53 -0700

    task1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: pointor
+ *
+ *Return: number of elements of a list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h != NULL)
+	{
+
+		h = h->next;
+		node++;
+	}
+
+	return (node);
+}
