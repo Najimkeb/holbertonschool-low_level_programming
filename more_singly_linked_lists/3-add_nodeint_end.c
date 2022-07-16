fa6be27a2317f5e24dc5128df490eeb57dabf143
Najimkeb <najimkeb31@gmail.com>
2022-07-16 12:04:37 -0700

    task 4

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,7 +4,6 @@
  * add_nodeint_end - adds a new node at the end of a list
  * @head: double pointer 
  * @n: variable
- *
  * Return: pointer to the new node
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
