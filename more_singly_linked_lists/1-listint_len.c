fb678fa937611769be948f1093f6182c5460fb13
Najimkeb <najimkeb31@gmail.com>
2022-07-16 11:25:31 -0700

    task 2

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked  list.
+ * listint_len - returns the number of elements in a linked list
  * @h: pointer 
  *
  * Return: number of elements in a linked  list
