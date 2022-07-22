4db3c6f2abbe726b8fa6981b04a980f4adf84df4
Najimkeb <najimkeb31@gmail.com>
2022-07-22 10:16:33 -0700

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,37 +10,25 @@
 int create_file(const char *filename, char *text_content)
 {
 
-	int a, b, d, len = 0;
-	char *c;
+	int a, d, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	a = open(filename, O_CREAT | O_RDWR, 0600);
+	a = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 
 	if (a < 0)
 		return (-1);
+
 	while (text_content && text_content[len])
 		len++;
 
-	c = malloc(sizeof(char) * len);
-	if (c == 0)
-		return (-1);
-	b = read(a, c, len);
-	if (b < 0)
-	{
-		free(c);
-		close(a);
-		return (-1);
-	}
-	d = write(STDOUT_FILENO, c, b);
+	d = write(a, text_content, len);
 	if (d < 0)
 	{
-		free(c);
 		close(a);
 		return (-1);
 	}
-	free(c);
 	close(a);
 	return (d);
 }
