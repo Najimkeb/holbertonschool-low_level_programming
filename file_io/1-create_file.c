f7cb2671cf28349b4cabfcbc2af49cc98ca0ea87
Najimkeb <najimkeb31@gmail.com>
2022-07-22 10:09:53 -0700

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -30,15 +30,17 @@ int create_file(const char *filename, char *text_content)
 	if (b < 0)
 	{
 		free(c);
+		close(a);
 		return (-1);
 	}
 	d = write(STDOUT_FILENO, c, b);
 	if (d < 0)
 	{
 		free(c);
+		close(a);
 		return (-1);
 	}
 	free(c);
-	return (d);
 	close(a);
+	return (d);
 }
