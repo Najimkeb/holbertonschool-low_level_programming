5325ed141320644a2b60fcb45e9c6bc49ef60867
Najimkeb <najimkeb31@gmail.com>
2022-07-22 09:22:24 -0700

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,9 +2,9 @@
 
 /**
  * create_file - creates a file
- *@b: variable
- *@a: variale
- *return: value
+ *@text_content: variable
+ *@filename: variale
+ *Return: create file value
  */
 
 int create_file(const char *filename, char *text_content)
@@ -13,32 +13,32 @@ int create_file(const char *filename, char *text_content)
 	int a, b, d, len = 0;
 	char *c;
 
-		if (filename == NULL)
-			return (-1);
+	if (filename == NULL)
+		return (-1);
 
-	a = open(filename, O_CREAT | O_RDWR);
+	a = open(filename, O_CREAT | O_RDWR, 0600);
 
-		if (a < 0)
-			return (-1);
-	while (text_content && text_content [len])
-			len ++;
+	if (a < 0)
+		return (-1);
+	while (text_content && text_content[len])
+		len++;
 
 	c = malloc(sizeof(char) * len);
 	if (c == 0)
 		return (-1);
-
-	b = read (a, c, len);
+	b = read(a, c, len);
 	if (b < 0)
 	{
 		free(c);
 		return (-1);
 	}
-
-	d = write (STDOUT_FILENO, c, b);
-	if ( d < 0)
+	d = write(STDOUT_FILENO, c, b);
+	if (d < 0)
 	{
 		free(c);
 		return (-1);
 	}
+	free(c);
 	return (d);
+	close(a);
 }
