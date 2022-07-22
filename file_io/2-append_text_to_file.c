e04760f2ce24a53580dadd05b9a4e39944ecd34c
Najimkeb <najimkeb31@gmail.com>
2022-07-22 10:50:43 -0700

    task 1

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -3,17 +3,17 @@
 /**
  * append_text_to_file - appends text at the end of a file.
  * @filename: variable
- * @text_content: The string 
+ * @text_content: The string
  * Return: 1 on success and -1 on failure
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int a, b, len;
+	int a, b, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	a= open(filename, O_APPEND  | O_RDWR);
+	a = open(filename, O_APPEND  | O_RDWR);
 	if (a == -1)
 		return (-1);
 	while (text_content && text_content[len] != '\0')
